9e0ebbac5e5fbb55664d68cdf24d92202ff7a437
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-07-28 23:20:11 -0400

    feat:add the login and register form

--- Frontend/src/User/Views/UserLogin.tsx
+++ Frontend/src/User/Views/UserLogin.tsx
@@ -1,7 +1,20 @@
 export function UserLogin() {
     return (
-        <div>
-            <h1>User login</h1>
+        <div className="text-center">
+            <h1 className="my-4">User login</h1>
+            <form className="grid justify-center" action="submit">
+                <label htmlFor="email">
+                    Email
+                    <input className="border-2 rounded-md block text-center" type="text" name="email" id="email" placeholder="ignacio.lara.v@usach.cl" />
+                </label>
+                <label htmlFor="password">
+                    Password
+                    <input className="border-2 rounded-md block text-center" type="text" name="password" id="password" placeholder="*******************" />
+                </label>
+                <button className="border-2 rounded-md block mt-4">
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
